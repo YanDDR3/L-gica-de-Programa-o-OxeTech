d8d46c54ca4e10172ac85500fb9ef449b409ea71
YanDDR3 <yan.msw@hotmail.com>
2026-06-19 16:27:25 -0300

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int idade  = 25; 
+    float altura = 1.73;
+    char genero = 'M'; 
+    char nome[] = "Yan";
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura); 
+    printf("Genero: %c \n", genero);
+    printf("Nome: %s\n", nome);
+    
+
+    return 0; 
+}
