8df6806fca96b7d5e864638f4add78eb6383e325
YanDDR3 <yan.msw@hotmail.com>
2026-06-29 19:28:19 -0300

    Add files via upload

---
+++ Aula 12/Aula 12 - Exercício 01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    
+    int numeros[5];
+
+    for (int indice = 0; indice < 5; indice++) {
+        printf("Digite o %d° numero: ", indice + 1);
+        scanf("%d", &numeros[indice]);
+    }
+
+    printf("\nLista dos numeros digitados: ");
+
+    for (int indice = 4; indice >= 0; indice--) {
+        printf("%d ", numeros[indice]);
+    }
+
+
+    return 0;
+}
